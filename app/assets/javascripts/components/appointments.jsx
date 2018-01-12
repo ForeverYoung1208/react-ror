b4ef91910bc1989dd0b1297e72ad9c4885179a36
Igor <siafin2010@gmail.com>
2018-01-12 18:44:32 +0200

    a

--- app/assets/javascripts/components/appointments.jsx
+++ app/assets/javascripts/components/appointments.jsx
@@ -1,7 +1,26 @@
 var Appointments = createReactClass({
+	getInitialState: function(){
+		return{
+			appointments: this.props.appointments,
+			title: 'Team blavlavla',
+			appt_time: 'Tomorrow at 9am'
+
+		}
+	},
+
+	handleUserInput: function(obj) {
+		this.setState(obj)
+	},
+
 	render: function(){
 		return (
-			<h1>CalReact </h1>
+			<div>
+				<AppointmentForm input_title={this.state.title} 
+					input_appt_time={this.state.appt_time} 
+					onUserInput={this.handleUserInput} />
+				<AppointmentsList appointments={this.state.appointments} />
+
+			</div>
 		)
 	}
 });
