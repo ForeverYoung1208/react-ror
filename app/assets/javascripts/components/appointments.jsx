14ab70c6b41d2a9d4a0d5c0defd750cd4b60e878
Igor <siafin2010@gmail.com>
2018-01-13 22:21:59 +0200

    a

--- app/assets/javascripts/components/appointments.jsx
+++ app/assets/javascripts/components/appointments.jsx
@@ -1,4 +1,5 @@
 var Appointments = createReactClass({
+	displayName: 'Appointments',
 	getInitialState: function(){
 		return{
 			appointments: this.props.appointments,
@@ -8,16 +9,41 @@ var Appointments = createReactClass({
 		}
 	},
 
+
 	handleUserInput: function(obj) {
 		this.setState(obj)
 	},
+	handleFormSubmit: function() {
+		var appointment = {
+			title: this.state.title,
+			appt_time: this.state.appt_time
+		}
+		console.log(appointment)
+		$.post('/appointments',
+						{appointment: appointment})
+					.done(function(data){
+						this.addNewAppointment(data)
+					}.bind(this));
+
+	},
+
+	addNewAppointment: function(appointment) {
+		var appointments = $.extend(true,[],this.state.appointments)
+		console.log(appointments)
+		appointments.push(appointment)
+		this.setState({ appointments: appointments })
+
+
+	},
+
 
 	render: function(){
 		return (
 			<div>
 				<AppointmentForm input_title={this.state.title} 
 					input_appt_time={this.state.appt_time} 
-					onUserInput={this.handleUserInput} />
+					onUserInput={this.handleUserInput} 
+					onFormSubmit={this.handleFormSubmit}/>
 				<AppointmentsList appointments={this.state.appointments} />
 
 			</div>
